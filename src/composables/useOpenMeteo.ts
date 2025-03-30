d2fccfb9a60482a12ea406f011cfb9b0da5894b0
Arshad Hasham <u11470@asambeauty.com>
2025-03-30 04:10:58 +0100

    fetch weather data and map temprature to time

--- src/composables/useOpenMeteo.ts
+++ src/composables/useOpenMeteo.ts
@@ -1,7 +1,9 @@
 import { ref } from 'vue'
+import { Weather } from '@/domain/entities/Weather';
+import { Temperature } from '@/domain/entities/Temprature';
 
 export function useOpenMeteo() {
-    const weatherData = ref([])
+    const weatherData = ref<Weather[]>([])
 
     const BASE_URL = `https://api.open-meteo.com/v1/forecast`;
 
@@ -29,8 +31,24 @@ export function useOpenMeteo() {
                 return
             }
 
-            weatherData.value = await response.json();
+            const responseInJson = await response.json();
+            weatherData.value = mapDateToTempratue(responseInJson)
             
+    }
+
+    const mapDateToTempratue = (data: any): Weather[] => {
+        const now = new Date();
+        const weatherData: Weather[] = [];
+        
+        for (let i = 0; i < data.hourly.time.length; i++) {
+            const date = new Date(data.hourly.time[i]);
+            const temperature = new Temperature(data.hourly.temperature_2m[i]);
+            const isForecast = date > now;
+            
+            weatherData.push(new Weather(date, temperature, isForecast));
+        }
+        
+        return weatherData;
     }
 
     return {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+import { useOpenMeteo } from '@/composables/useOpenMeteo';
+
+const { weatherData, getWeatherData } = useOpenMeteo();
+
+getWeatherData()
+
 </script>
 
 <template>
   <main>
+    <div v-if="weatherData.length > 0">
+      <div v-for="(weather, index) in weatherData" :key="index">
+            <p>{{ weather.date }}</p>
+            <p>{{ weather.temperature }}</p>
+            <p>
+              {{ weather.isForecast ? 'Forecast' : 'Historical' }}
+            </p>
+          </div>
+    </div>
   </main>
 </template>
