b4390aa72af0753c8b648765a50871178bd3ace6
Arshad Hasham <u11470@asambeauty.com>
2025-03-30 05:00:43 +0100

    added open meteo composable for weather search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useOpenMeteo } from '@/composables/useOpenMeteo';
+import HomeWeatherTable from '@/components/home/HomeWeatherTable.vue';
 
 const { weatherData, getWeatherData } = useOpenMeteo();
 
-getWeatherData()
+// i am using lisbon as default
+const latitude = ref(38.713);
+const longitude = ref(-9.139);
+
+getWeatherData(latitude.value, longitude.value)
 
 </script>
 
 <template>
   <main>
     <div v-if="weatherData.length > 0">
-      <div v-for="(weather, index) in weatherData" :key="index">
-            <p>{{ weather.date }}</p>
-            <p>{{ weather.temperature }}</p>
-            <p>
-              {{ weather.isForecast ? 'Forecast' : 'Historical' }}
-            </p>
-          </div>
+      <HomeWeatherTable :weather-data="weatherData"/>
     </div>
   </main>
 </template>
